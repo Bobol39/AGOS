4f262ee9055aeb419807554794123f0bff5ff37f
Bobol <clementdujura@gmail.com>
2016-10-06 16:40:51 +0200

    ajout vue index

--- application/controllers/c_index.php
+++ application/controllers/c_index.php
@@ -20,6 +20,7 @@ class C_index extends CI_Controller {
 	 */
 	public function index()
 	{
+		$this->load->helper('form');
 		$this->load->view('v_index');
 	}
 
@@ -38,11 +39,6 @@ class C_index extends CI_Controller {
 			if(($donnees_session=$this->Users_model->verif_connexion($donnees)) != False)// and valide ==1
 			{
 				$this->session->set_userdata($donnees_session);
-				if ($donnees_session['droit'] == 0) {
-					redirect('Users');
-				}else{
-					redirect('Admin');
-				}
 			}
 			else{
 				$donnees['erreur']="mot de passe ou login incorrect";
