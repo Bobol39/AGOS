bfe88e8a0407710e08b5f42dc9a4316b653128ed
Pelomedusa <alfredossic@hotmail.fr>
2016-10-07 16:56:20 +0200

    notation

--- application/views/v_soutenance.php
+++ application/views/v_soutenance.php
@@ -1,5 +1,22 @@
 <link href="<?=base_url();?>assets/css/soutenance.css" rel="stylesheet">
 
+<div class="col-lg-12 col-md-12" id="container_notation">
+    <div class="col-lg-12 col-md-12" id="block_notation">
+        <div class="col-lg-3 col-md-3" style="height: 100%"></div>
+        <div class="col-lg-9 col-md-9" style="height: 100%">
+            <table>
+                <tr>
+                    <th valign="top">Insuffisant</th>
+                    <th valign="top">Superficiel</th>
+                    <th valign="top">Moyen</th>
+                    <th valign="top">Maîtrisé</th>
+                    <th valign="top">N.E</th>
+                </tr>
+            </table>
+        </div>
 
-
-<textarea id="text_aera"></textarea>
+    </div>
+    <div class="col-lg-12 col-md-12" id="block_textarea">
+        <textarea></textarea>
+    </div>
+</div>
