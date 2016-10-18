dcb90d86058a20a7c132531e5cd771473cc526af
Bobol <clementdujura@gmail.com>
2016-10-18 18:24:22 +0200

    modif de la bdd

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -9,8 +9,8 @@
     <link href="<?=base_url();?>assets/css/global.css" rel="stylesheet">
     <link rel="stylesheet" href="<?=base_url();?>assets/css/jquery-ui.css">
 
-    <script src="<?=base_url();?>assets/js/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-    <script src="<?=base_url();?>assets/js/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+    <script src="<?=base_url();?>assets/js/jquery-3.1.1.min.js"  crossorigin="anonymous"></script>
+    <script src="<?=base_url();?>assets/js/jquery-ui.min.js" crossorigin="anonymous"></script>
     <script type="text/javascript" src="<?=base_url();?>assets/js/bootstrap.min.js"> </script>
     <script type="text/javascript" src="<?=base_url();?>assets/js/global.js"> </script>
 
