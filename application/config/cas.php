5f393c69fcc0221d13973dab765f4f3a445b070a
pelomedusa <alfred.jossic@edu.univ-fcomte.fr>
2016-10-23 17:27:02 +0200

    CAS installé comme des sauvages, 1/10 chances que ca marche

---
+++ application/config/cas.php
@@ -0,0 +1,5 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$config['cas_server_url'] = 'https://cas.univ-fcomte.fr/cas';
+$config['phpcas_path'] = 'C:\wamp\www\AGOS\application\libraries\CAS-1.3.4';
+$config['cas_disable_server_validation'] = TRUE;
+$config['cas_debug'] = TRUE; // <--  use this to enable phpCAS debug mode
