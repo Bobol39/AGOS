1bb3261c654bd7ba75ce55ea7a7ac06c94fd50b4
Pelomedusa <alfredossic@hotmail.fr>
2016-10-24 09:22:52 +0200

    Ajout des fichiers parce que ilyes sait pas faire

---
+++ application/controllers/c_cas.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Yeso
+ * Date: 23/10/2016
+ * Time: 19:42
+ */
+class c_cas extends MY_LoginControl
+{
+    protected $access = "Admin";
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+        $this->load->helper(array('form', 'url', 'text', 'string'));
+        $this->load->library(array('session', 'form_validation', 'email'));
+//		$this->load->model('Users_model');
+    }
+
+    function index(){
+        //$this->cas->force_auth();
+        //$this->cas->user();
+        $this->load->view("v_cas");
+    }
+
+}
