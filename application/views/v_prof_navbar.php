965abab7497ba40bab41546edb41dee6dff842a0
Bobol <clementdujura@gmail.com>
2017-02-14 16:56:31 +0100

    nav bar prof modif

--- application/views/v_prof_navbar.php
+++ application/views/v_prof_navbar.php
@@ -14,10 +14,19 @@
     <div class="col-lg-1 col-md-1 button_navbar_notation">
         <span>AGOS</span>
     </div>
+    <?php if ($_SESSION['role'] == 'admin'){ ?>
+        <a href="<?php echo base_url('index.php/c_admin'); ?>">
+            <div class="col-lg-2 col-md-2 button_navbar_notation">
+                <span>Espace Admin</span>
+            </div>
+        </a>
+    <?php }else{ ?>
+        <div class="col-lg-2 col-md-2"></div>
+    <?php } ?>
 
 
     <a href="<?php echo base_url('index.php/c_prof/'); ?>">
-        <div class="col-lg-2 col-lg-offset-7 col-md-2 col-md-offset-7 button_navbar_notation">
+        <div class="col-lg-2 col-lg-offset-5 col-md-2 col-md-offset-7 button_navbar_notation">
             <span>Evaluer</span>
         </div>
     </a>
