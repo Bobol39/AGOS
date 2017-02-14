1b38938527dd4de1db0a29c0cfa886fc6a079a5f
Pelomedusa <alfredossic@hotmail.fr>
2017-02-14 21:10:42 +0100

    vue recap

--- assets/js/edition_notes.js
+++ assets/js/edition_notes.js
@@ -8,7 +8,6 @@ $(function () {
     $("#selectGroupe").change(function () {
         $("#tableShow tbody").html("");
         $("#tableEditNote tbody").html("");
-        $("#btnAnnotations").hide();
         var val = $(this).find("option:selected").val();
         if (val != 0){
             $('#button_export').fadeIn();
@@ -20,6 +19,7 @@ $(function () {
             }).done( function(data){
                 data = JSON.parse(data)
                 console.log(data);
+
                 data.forEach(function (sout) {
                     if (sout.notes.length != 0){
                         var ligne = $("<tr><td>"+sout.titre+"</td><td>"+sout.id_etudiant+"</td><td>"+sout.date+"</td><td>"+sout.horaire+"</td><td>"+sout.id_salle+"</td><td>"+sout.professeur1+"/"+sout.professeur2+"</td><td>"+calcNote(sout.notes)+"/20</td><td><a href='"+baseurl+"index.php/c_prof/showRecap/"+sout.id+"'>Details</a></td></tr>")
@@ -60,7 +60,6 @@ function editNotes(sout) {
         });
         $("#tableEditNote tbody").append(line);
     })
-    $("#btnAnnotations").fadeIn();
     $("#btnValider").off("click").click(function () {
         if (checkBeforeSave()){
             start_loading();
